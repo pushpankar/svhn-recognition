cbc9cd2b40117080c2ba74b2ee4f950dfdbc881d
Pushpankar <pushpankark@gmail.com>
2017-02-04 01:45:30 +0530

    Code reformatting

--- svhn_classification.py
+++ svhn_classification.py
@@ -1,17 +1,26 @@
 import tensorflow as tf
 import numpy as np
-from svhn_data import get_test_data, train_and_validation_data,get_train_data
+from svhn_data import get_train_data
 
-def weight_variable(shape):
+
+def weight_var(shape):
     return tf.Variable(tf.truncated_normal(shape, stddev=0.1))
 
-def bias_variable(shape):
+
+def bias_var(shape):
     return tf.Variable(tf.constant(1.0, shape=shape))
-def accuracy(predictions, labels):
-    return (100.0 * np.sum(np.argmax(predictions, 2) == np.argmax(labels, 2))/ predictions.shape[0])/6
+
+
+def accuracy(pred, labels):
+    return (100.0 * np.sum(
+        np.argmax(pred, 2) == np.argmax(labels, 2)) / pred.shape[0])/6
+
+
+def conv2d(data, wt, bias, stride=[1, 2, 2, 1]):
+    return tf.nn.relu(tf.nn.conv2d(data, wt, stride, padding='SAME') + bias)
+
 
 offset = 0
-
 image_height = 32
 image_width = 128
 num_channels = 1
@@ -27,122 +36,144 @@ reg_hidden1 = 2048
 reg_hidden2 = 1024
 reg_hidden3 = 512
 
-Xvalidation, yvalidation, bbox = get_train_data('train/',offset,batch_size)
+Xvalid, yvalid, bbox_valid = get_train_data('train/', offset, batch_size)
+offset += batch_size
+Xtest, ytest, bbox_test = get_train_data('train/', offset, batch_size)
 offset += batch_size
 
-#print dimensions
-print("Xvalidation shape is {} and yvalidation shape is {} and bbox is {}".format(Xvalidation.shape, yvalidation.shape, bbox.shape))
-print('bbox is {}'.format(bbox[0]))
 
-#build a graph
+# print dimensions
+print("Xvalid shape is {} and yvalid shape is {} and bbox is {}"
+      .format(Xvalid.shape, yvalid.shape, bbox_valid.shape))
+print('bbox is {}'.format(bbox_valid[0]))
+
+# build a graph
 graph = tf.Graph()
 with graph.as_default():
-    tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, image_width, image_height, num_channels))
-    tf_train_labels  = tf.placeholder(tf.float32, shape=(batch_size, num_digits, num_labels))
-    tf_valid_dataset = tf.cast(tf.constant(Xvalidation),tf.float32)
-    tf_train_bbox = tf.placeholder(tf.float32, shape=(batch_size, num_digits,4))
-    #tf_test_dataset = tf.constant(Xtest)
+    tf_train_dataset = tf.placeholder(
+        tf.float32, shape=(batch_size,
+                           image_width, image_height, num_channels))
+    tf_train_labels = tf.placeholder(
+        tf.float32, shape=(batch_size, num_digits, num_labels))
+    tf_valid_dataset = tf.cast(tf.constant(Xvalid), tf.float32)
+    tf_train_bbox = tf.placeholder(tf.float32,
+                                   shape=(batch_size, num_digits, 4))
+    tf_test_dataset = tf.constant(Xtest)
 
-    #Create variables
-    #convolutions layer 1
-    layer1_W = weight_variable([patch_size, patch_size, num_channels, depth//2])
-    layer1_bias = bias_variable([depth//2])
+    # Create variables
+    # convolutions layer 1
+    layer1_W = weight_var([patch_size, patch_size, num_channels, depth//2])
+    layer1_bias = bias_var([depth//2])
 
-    #layer2
-    layer2_W = weight_variable([patch_size, patch_size, depth//2, depth//2])
-    layer2_bias = bias_variable([depth//2])
+    # layer2
+    layer2_W = weight_var([patch_size, patch_size, depth//2, depth//2])
+    layer2_bias = bias_var([depth//2])
 
-    #layer3
-    layer3_W = weight_variable([patch_size, patch_size, depth//2, depth])
-    layer3_bias = bias_variable([depth])
+    # layer3
+    layer3_W = weight_var([patch_size, patch_size, depth//2, depth])
+    layer3_bias = bias_var([depth])
 
-    #layer3a
-    layer3a_W = weight_variable([patch_size, patch_size, depth, depth])
-    layer3a_bias = bias_variable([depth])
+    # layer4
+    layer4_W = weight_var([patch_size, patch_size, depth, depth])
+    layer4_bias = bias_var([depth])
 
-    #layer3
-    layer3c_W = weight_variable([patch_size, patch_size, depth, depth])
-    layer3c_bias = bias_variable([depth])
-    
+    # layer5
+    layer5_W = weight_var([patch_size, patch_size, depth, depth])
+    layer5_bias = bias_var([depth])
 
-    #Regression head
-    fc1_Reg_W = weight_variable([image_height//8*image_width//8*depth, reg_hidden1])
-    fc1_reg_bias = bias_variable([reg_hidden1])
+    # Regression head
+    fc1_Reg_W = weight_var([image_height//8*image_width//8*depth, reg_hidden1])
+    fc1_reg_bias = bias_var([reg_hidden1])
 
-    fc2_reg_W = weight_variable([reg_hidden1, reg_hidden2])
-    fc2_reg_bias = bias_variable([reg_hidden2])
+    fc2_reg_W = weight_var([reg_hidden1, reg_hidden2])
+    fc2_reg_bias = bias_var([reg_hidden2])
 
-    fc2b_reg_W = weight_variable([reg_hidden2, reg_hidden3])
-    fc2b_reg_bias = bias_variable([reg_hidden3])
+    fc3_reg_W = weight_var([reg_hidden2, reg_hidden3])
+    fc3_reg_bias = bias_var([reg_hidden3])
 
-    fc3_reg_Ws = [weight_variable([reg_hidden3, 4]) for _ in xrange(num_digits)]
-    fc3_reg_biases =[bias_variable([4]) for _ in xrange(num_digits)]
+    fc4_reg_Ws = [weight_var([reg_hidden3, 4]) for _ in range(num_digits)]
+    fc4_reg_biases = [bias_var([4]) for _ in range(num_digits)]
 
+    # Classification head
+    # layer 4
+    c1_W = weight_var([image_height//8*image_width//8*depth, num_hidden1])
+    c1_bias = bias_var([num_hidden1])
 
-    #Classification head
-    #layer 4
-    layer4_W = weight_variable([image_height//8*image_width//8*depth, num_hidden1])
-    layer4_bias = bias_variable([num_hidden1])
+    c2_W = weight_var([num_hidden1, num_hidden2])
+    c2_bias = bias_var([num_hidden2])
 
-    layer4a_W = weight_variable([num_hidden1, num_hidden2])
-    layer4a_bias = bias_variable([num_hidden2])
-    #layer 5
-    layer5_Ws = [weight_variable([num_hidden2, num_labels]) for _ in xrange(num_digits)]
-    layer5_biases = [bias_variable([num_labels]) for _ in xrange(num_digits)]
+    # layer 5
+    c3_Ws = [weight_var([num_hidden2, num_labels])
+             for _ in range(num_digits)]
+    c3_biases = [bias_var([num_labels]) for _ in range(num_digits)]
 
-    #Design model
+    # Design model
     def model(data):
-        conv1 = tf.nn.relu(tf.nn.conv2d(data, layer1_W, [1,2,2,1], padding='SAME') + layer1_bias)
-        conv2 = tf.nn.relu(tf.nn.conv2d(conv1, layer2_W, [1,2,2,1], padding='SAME') + layer2_bias)
-        conv3 = tf.nn.relu(tf.nn.conv2d(conv2, layer3_W, [1,2,2,1], padding='SAME') + layer3_bias)
-        conv3a = tf.nn.relu(tf.nn.conv2d(conv3, layer3a_W, [1,1,1,1], padding='SAME') + layer3a_bias)
-        conv3c = tf.nn.relu(tf.nn.conv2d(conv3a, layer3c_W, [1,1,1,1], padding='SAME') + layer3c_bias)
-        shape = conv3c.get_shape().as_list()
-        reshaped = tf.reshape(conv3c, [shape[0], shape[1] * shape[2] * shape[3]])
+        conv1 = conv2d(data, layer1_W, layer1_bias)
+        conv2 = conv2d(conv1, layer2_W, layer2_bias)
+        conv3 = conv2d(conv2, layer3_W, layer3_bias)
+        conv4 = conv2d(conv3, layer4_W, layer4_bias, stride=[1, 1, 1, 1])
+        conv5 = conv2d(conv4, layer5_W, layer5_bias, stride=[1, 1, 1, 1])
+        shape = conv5.get_shape().as_list()
+        reshaped = tf.reshape(conv5,
+                              [shape[0], shape[1] * shape[2] * shape[3]])
 
-        #Regression head for bounding box prediction
+        # Regression head for bounding box prediction
         reg1 = tf.nn.relu(tf.matmul(reshaped, fc1_Reg_W) + fc1_reg_bias)
         reg2 = tf.nn.relu(tf.matmul(reg1, fc2_reg_W) + fc2_reg_bias)
-        reg2b = tf.nn.relu(tf.matmul(reg2, fc2b_reg_W) + fc2b_reg_bias)
-        bbox_pred = tf.pack([tf.matmul(reg2b, fc3_reg_W) + fc3_reg_bias for fc3_reg_W, fc3_reg_bias in zip(fc3_reg_Ws, fc3_reg_biases)])
-        bbox_pred = tf.transpose(bbox_pred, [1,0,2])
+        reg3 = tf.nn.relu(tf.matmul(reg2, fc3_reg_W) + fc3_reg_bias)
+        bbox_pred = [tf.matmul(reg3, fc3_reg_W) + fc3_reg_bias
+                     for fc3_reg_W, fc3_reg_bias
+                     in zip(fc4_reg_Ws, fc4_reg_biases)]
+        bbox_pred = tf.transpose(tf.pack(bbox_pred), [1, 0, 2])
 
-        #classification head
-        hidden1 = tf.nn.relu(tf.matmul(reshaped, layer4_W) + layer4_bias)
-        hidden2 = tf.nn.relu(tf.matmul(hidden1, layer4a_W) + layer4a_bias)
-        logits = tf.pack([tf.matmul(hidden2, layer5_W) + layer5_bias for layer5_W, layer5_bias in zip(layer5_Ws, layer5_biases)])
-        logits = tf.transpose(logits, [1,0,2])
+        # classification head
+        hidden1 = tf.nn.relu(tf.matmul(reshaped, c1_W) + c1_bias)
+        hidden2 = tf.nn.relu(tf.matmul(hidden1, c2_W) + c2_bias)
+        logits = tf.pack([tf.matmul(hidden2, c3_W) + c3_bias
+                          for c3_W, c3_bias in zip(c3_Ws, c3_biases)])
+        logits = tf.transpose(logits, [1, 0, 2])
         return logits, bbox_pred
 
     logits, bbox_pred = model(tf_train_dataset)
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
+    loss = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
     bbox_loss = tf.nn.l2_loss(bbox_pred - tf_train_bbox)
-   
-    #optimizer
-    global_step = tf.Variable(0,trainable=False)
+
+    # optimizer
+    global_step = tf.Variable(0, trainable=False)
     starter_learning_rate = 0.005
-    learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,5,0.90,staircase=True)
-    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss,global_step=global_step)
+    learning_rate = tf.train.exponential_decay(starter_learning_rate,
+                                               global_step, 5, 0.90,
+                                               staircase=True)
+    optimizer = tf.train.AdamOptimizer(
+        learning_rate).minimize(loss, global_step=global_step)
     bbox_optimizer = tf.train.AdamOptimizer(0.001).minimize(bbox_loss)
 
-    
-    train_prediction = tf.nn.softmax(logits)
-    validation_prediction = tf.nn.softmax(model(tf_valid_dataset)[0])
-#    test_prediction = tf.nn.softmax(model(tf_test_dataset))
+    # Predictions
+    train_pred = tf.nn.softmax(logits)
+    valid_pred = tf.nn.softmax(model(tf_valid_dataset)[0])
+    test_pred = tf.nn.softmax(model(tf_test_dataset))
 
 with tf.Session(graph=graph) as session:
     tf.global_variables_initializer().run()
     print('Initialized')
-    
 
     for step in range(1001):
-        batch_data, batch_label, bbox = get_train_data('train/',offset,batch_size)
+        batch_data, batch_label, bbox = get_train_data('train/',
+                                                       offset, batch_size)
         offset += batch_size
-        feed_dict = { tf_train_dataset: batch_data,
-                      tf_train_labels : batch_label,
-                      tf_train_bbox   : bbox }
-        _, l, predictions,_,bbox_cost = session.run([optimizer, loss, train_prediction,bbox_optimizer,bbox_loss], feed_dict = feed_dict)
-        if step%1 == 0:
-            print('Minibatch accuracy at step {} : {} and loss is {} with rate {}'.format(step, accuracy(predictions, batch_label),l,learning_rate))
-            print('Validation accuracy is {}'.format(accuracy(validation_prediction.eval(), yvalidation)))
-            print('Regression loss is {} is {}'.format(step,bbox_cost))                                                 
+        feed_dict = {tf_train_dataset: batch_data,
+                     tf_train_labels: batch_label,
+                     tf_train_bbox: bbox}
+        _, l, predictions, _, bbox_cost = session.run(
+            [optimizer, loss, train_pred, bbox_optimizer, bbox_loss],
+            feed_dict=feed_dict)
+        if step % 10 == 0:
+            print('Minibatch accuracy at step {} : {} and loss is {}'.format(
+                step, accuracy(predictions, batch_label), l))
+            print('Validation accuracy is {}'.format(
+                accuracy(valid_pred.eval(), yvalid)))
+            print('Regression loss is {} is {}'.format(step, bbox_cost))
+
+    print('Test accuracy is {}'.format(accuracy(test_pred.eval(), ytest)))
--- svhn_data.py
+++ svhn_data.py
@@ -1,19 +1,18 @@
-from scipy.io import loadmat
 import h5py
 import os.path
 import numpy as np
 import cPickle as pickle
 from os import listdir
 from PIL import Image
-from sklearn.model_selection import train_test_split
 
 f = h5py.File('train/digitStruct.mat')
-metadata= {}
+metadata = {}
 metadata['height'] = []
 metadata['label'] = []
 metadata['left'] = []
 metadata['top'] = []
 metadata['width'] = []
+
 
 def print_attrs(name, obj):
     vals = []
@@ -24,39 +23,42 @@ def print_attrs(name, obj):
             vals.append(f[obj[k][0]][0][0])
     metadata[name].append(vals)
 
-def prepare_data():
+
+def prepare_data(path):
+    f = h5py.File('train/digitStruct.mat')
     for item in f['/digitStruct/bbox']:
         f[item[0]].visititems(print_attrs)
 
-    pickle_file = 'train/train_metadata.pickle'
+    pickle_file = path+'metadata.pickle'
     try:
-        with open(pickle_file, 'wb') as f:
-            f.dump(metadata, pickle_file)
+        with open(pickle_file, 'wb') as pf:
+            pickle.dump(metadata, pf)
     except Exception as e:
-        print 'Unable to save data to', pickle_file, ':', e
+        print('Unable to save data to', pickle_file, ':', e)
         raise
 
 
 def one_hot_encode(labels):
-    b = np.zeros((len(labels),6,11))
-    b[:,:,10] = 1
+    b = np.zeros((len(labels), 6, 11))
+    b[:, :, 10] = 1
     for img_num in range(len(labels)):
         for index, num in enumerate(labels[img_num]):
-            b[img_num,index,num] = 1
-            b[img_num,index,10] = 0
+            b[img_num, index, num] = 1
+            b[img_num, index, 10] = 0
     return b
+
 
 def get_bounding_box_as_array(metadata, offset, batch_size):
     metadata['top'] = metadata['top'][offset:offset+batch_size]
     metadata['left'] = metadata['left'][offset:offset+batch_size]
     metadata['height'] = metadata['height'][offset:offset+batch_size]
     metadata['width'] = metadata['width'][offset:offset+batch_size]
-    
-    bbox = np.zeros((batch_size,6,4))
+    bbox = np.zeros((batch_size, 6, 4))
+
     for key in metadata:
         if key != 'label':
-            for img_num in xrange(batch_size):
-                for index,val in enumerate(metadata[key][img_num]):
+            for img_num in range(batch_size):
+                for index, val in enumerate(metadata[key][img_num]):
                     if key == 'top':
                         label_num = 0
                     elif key == 'left':
@@ -65,43 +67,34 @@ def get_bounding_box_as_array(metadata, offset, batch_size):
                         label_num = 2
                     elif key == 'width':
                         label_num = 3
-                    bbox[img_num,index,label_num] = val
+                    bbox[img_num, index, label_num] = val
     return bbox
-   
+
+
 def get_train_data(path, offset, batch_size):
-    if not os.path.exists(path + 'train_metadata.pickle'):
-        prepare_data()
-    with open('train/train_metadata.pickle', 'rb') as f:
+    if not os.path.exists(path + 'metadata.pickle'):
+        prepare_data(path)
+    with open(path + 'metadata.pickle', 'rb') as f:
         metadata = pickle.load(f)
 
     if not os.path.isfile(path+'imagelist.pickle'):
-        imagelist = filter(lambda x:'png' in x, listdir(path))
-        imagelist = sorted(imagelist, key=lambda x: int(filter(str.isdigit, x)))
+        imagelist = filter(lambda x: 'png' in x, listdir(path))
+        imagelist = sorted(imagelist,
+                           key=lambda x: int(filter(str.isdigit, x)))
         with open(path+'imagelist.pickle', 'wb') as f:
-            pickle.dump(imagelist,f)
+            pickle.dump(imagelist, f)
     else:
-        with open(path+'imagelist.pickle','rb') as f:
+        with open(path+'imagelist.pickle', 'rb') as f:
             imagelist = pickle.load(f)
-                
+
     loaded_images = []
     for image in imagelist[offset:offset+batch_size]:
-        img = Image.open(path+image).convert('L').resize((128,32),Image.BILINEAR)
+        img = Image.open(path+image).convert('L').resize(
+            (128, 32), Image.BILINEAR)
         im = np.asarray(img)
-        loaded_images.append(im.reshape(128,32,1))
-        
+        loaded_images.append(im.reshape(128, 32, 1))
+
     ytrain = metadata['label'][offset:offset+batch_size]
     ytrain = one_hot_encode(ytrain)
     bbox = get_bounding_box_as_array(metadata, offset, batch_size)
     return np.array(loaded_images), np.array(ytrain), bbox
-
-def get_test_data(path):
-    test = loadmat(path+"test_32x32.mat")
-    Xtest = np.rollaxis(test['X'].astype(np.float32),3)
-    ytest  = one_hot_encode(test['y'])
-    return Xtest, ytest
-
-def train_and_validation_data(path):
-    #train test split
-    data, labels = get_train_data(path,)
-    return train_test_split(data, labels, test_size=0.025, random_state=8)
-
